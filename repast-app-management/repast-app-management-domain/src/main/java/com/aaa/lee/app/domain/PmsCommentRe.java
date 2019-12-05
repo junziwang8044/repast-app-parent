82f381fbb5a9adb85e5737580b4449c9272dd921
2398586425 <57249526+2398586425@users.noreply.github.com>
2019-12-05 14:14:23 +0800

    更多搜索历史更新

--- repast-app-management/repast-app-management-domain/src/main/java/com/aaa/lee/app/domain/PmsCommentRe.java
+++ repast-app-management/repast-app-management-domain/src/main/java/com/aaa/lee/app/domain/PmsCommentRe.java
@@ -1,32 +1,18 @@
 package com.aaa.lee.app.domain;
 
 import javax.persistence.*;
-import java.util.Date;
 
-@Table(name = "pms_comment_replay")
+@Table(name = "delete_history_search")
 public class PmsCommentRe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "comment_id")
-    private Long commentId;
+    @Column(name = "history_search_name")
+    private String historySearchName;
 
-    @Column(name = "member_nick_name")
-    private String memberNickName;
-
-    @Column(name = "member_icon")
-    private String memberIcon;
-
-    private String content;
-
-    @Column(name = "create_time")
-    private Date createTime;
-
-    /**
-     * 评论人员类型；0->会员；1->管理员
-     */
-    private Integer type;
+    @Column(name = "member_id")
+    private Long memberId;
 
     /**
      * @return id
@@ -43,90 +29,30 @@ public class PmsCommentRe {
     }
 
     /**
-     * @return comment_id
+     * @return history_search_name
      */
-    public Long getCommentId() {
-        return commentId;
+    public String getHistorySearchName() {
+        return historySearchName;
     }
 
     /**
-     * @param commentId
+     * @param historySearchName
      */
-    public void setCommentId(Long commentId) {
-        this.commentId = commentId;
+    public void setHistorySearchName(String historySearchName) {
+        this.historySearchName = historySearchName == null ? null : historySearchName.trim();
     }
 
     /**
-     * @return member_nick_name
+     * @return member_id
      */
-    public String getMemberNickName() {
-        return memberNickName;
+    public Long getMemberId() {
+        return memberId;
     }
 
     /**
-     * @param memberNickName
+     * @param memberId
      */
-    public void setMemberNickName(String memberNickName) {
-        this.memberNickName = memberNickName == null ? null : memberNickName.trim();
-    }
-
-    /**
-     * @return member_icon
-     */
-    public String getMemberIcon() {
-        return memberIcon;
-    }
-
-    /**
-     * @param memberIcon
-     */
-    public void setMemberIcon(String memberIcon) {
-        this.memberIcon = memberIcon == null ? null : memberIcon.trim();
-    }
-
-    /**
-     * @return content
-     */
-    public String getContent() {
-        return content;
-    }
-
-    /**
-     * @param content
-     */
-    public void setContent(String content) {
-        this.content = content == null ? null : content.trim();
-    }
-
-    /**
-     * @return create_time
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * @param createTime
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取评论人员类型；0->会员；1->管理员
-     *
-     * @return type - 评论人员类型；0->会员；1->管理员
-     */
-    public Integer getType() {
-        return type;
-    }
-
-    /**
-     * 设置评论人员类型；0->会员；1->管理员
-     *
-     * @param type 评论人员类型；0->会员；1->管理员
-     */
-    public void setType(Integer type) {
-        this.type = type;
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
     }
 }
